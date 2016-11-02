aafdef20c3bb6d570e2f2d32250eb8f8e954df62
Andrey <paction@bk.ru>
2016-11-02 15:03:38 -0400

    footer template

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,7 @@
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.js"></script>
