7768d78e84c44e5a13dcb4528c969788e4234c4a
Andrey <paction@bk.ru>
2016-11-02 13:19:14 -0400

    product list and details

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    /**
+    * Shows the list of products.
+    *
+    * @param  Request  $request
+    * @return Response
+    */
+    public function index(Request $request)
+    {
+        $id = (int)$request->input('id');
+        if(!$id) {
+            $products = App\Products::all();
+            
+            return view('products.index', [
+                'products' => $products,
+            ]);
+        } else {
+            $product = App\Products::find($id);
+            
+            if($product) {
+                return view('products.details', [
+                    'product' => $product,
+                ]);
+            } else {
+                //error
+            }
+        }
+    }
 }
