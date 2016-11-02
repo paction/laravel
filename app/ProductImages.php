397a1b1f45ba046e9497fac6e2e7ac76dbf5d05e
Andrey <paction@bk.ru>
2016-11-02 11:45:22 -0400

    relation to Products

--- app/ProductImages.php
+++ app/ProductImages.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImages extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Products::class);
+    }
 }
