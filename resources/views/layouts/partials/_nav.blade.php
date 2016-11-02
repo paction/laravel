66c1820db97322ec5eac9590e642c693fd974a12
Andrey <paction@bk.ru>
2016-11-02 14:38:52 -0400

    navigation

---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header page-scroll">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Laravel</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="/products">Products</a>
+        </li>
+        <li>
+          <a href="/cart">Cart</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
