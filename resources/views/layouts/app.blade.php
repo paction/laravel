4bb0a664a5f7a37b171ffe8fd0dbb1f4a5a367d8
Andrey <paction@bk.ru>
2016-11-02 14:34:20 -0400

    app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <title>Laravel Assessment - @yield('title')</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
   </head>
 
