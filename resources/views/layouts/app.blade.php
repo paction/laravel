3d085705d149a6d61c605d2bb662261cf37dceaa
Andrey <paction@bk.ru>
2016-11-02 14:32:25 -0400

    extending layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,17 @@
   <head>
     <title>Laravel Assessment - @yield('title')</title>
 
-    <!-- CSS & JavaScript -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
   </head>
 
   <body>
-    <div class="container">
-      <nav class="navbar navbar-default">
-        <!-- Navbar -->
-      </nav>
-    </div>
-
+    @include('layouts.partials._nav')
+    
+    @include('layouts.partials._header')
+    
     @yield('content')
+    
+    @include('layouts.partials._footer')
   </body>
 </html>
