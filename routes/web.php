cc53ae6869cbc4fde508be17fa8d77e026ec0182
Andrey <paction@bk.ru>
2016-11-02 12:52:50 -0400

    additional routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
