fc68f3fac39f5cea7d136c968f3d87a10fc02899
Andrey <paction@bk.ru>
2016-11-02 14:26:08 -0400

    yield title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Laravel Assessment</title>
+    <title>Laravel Assessment - @yield('title')</title>
 
     <!-- CSS & JavaScript -->
   </head>
