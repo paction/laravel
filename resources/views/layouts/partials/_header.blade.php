10060a8d9ff16b5388f82492c87693770c597c37
Andrey <paction@bk.ru>
2016-11-02 14:40:25 -0400

    header

---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,8 @@
+<header>
+  <div class="container">
+    <div class="intro-text">  
+      <div class="intro-lead-in">Welcome To Our Laravel Store!</div>
+      <div class="intro-heading">It's Nice To Meet You</div>
+    </div>
+  </div>
+</header>
