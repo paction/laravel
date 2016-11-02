624476b142a80c6e7c9745345664bd7f8744aff3
Andrey <paction@bk.ru>
2016-11-02 12:56:12 -0400

    app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Laravel Assessment</title>
+
+    <!-- CSS & JavaScript -->
+  </head>
+
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <!-- Navbar -->
+      </nav>
+    </div>
+
+    @yield('content')
+  </body>
+</html>
